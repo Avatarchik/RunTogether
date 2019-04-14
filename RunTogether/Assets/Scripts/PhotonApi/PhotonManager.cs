36ce17e8a54293b36d7fb787def019c0974a7ce7
GUI <446824106@qq.com>
2019-04-14 13:09:08 +0800

    add photon

---
+++ RunTogether/Assets/Scripts/PhotonApi/PhotonManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+public class PhotonManager : MonoBehaviourPunCallbacks
+{
+    public byte Version = 1;
+    public GameObject playerPrefab;
+    public Transform bg;
+
+    public void PhotonConnent()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        PhotonNetwork.GameVersion = this.Version + "." + SceneManagerHelper.ActiveSceneBuildIndex;
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster() was called by PUN. Now this client is connected and could join a room. Calling: PhotonNetwork.JoinRandomRoom();");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby(). This client is connected and does get a room-list, which gets stored as PhotonNetwork.GetRoomList(). This script now calls: PhotonNetwork.JoinRandomRoom();");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one. Calling: PhotonNetwork.CreateRoom(null, new RoomOptions() {maxPlayers = 4}, null);");
+        PhotonNetwork.CreateRoom(null, new RoomOptions() { MaxPlayers = 4 }, null);
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.Log("OnDisconnected(" + cause + ")");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room. From here on, your game would be running.");
+        if (playerPrefab != null)
+        {
+            GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, playerPrefab.transform.position, Quaternion.identity, 0);
+        }
+    }
+}
---
+++ RunTogether/Assets/Scripts/PhotonApi/PlayController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayController : MonoBehaviour
+{
+    public RectTransform player;
+    public PhotonView view;
+
+    void Start()
+    {
+        view = GetComponent<PhotonView>();
+        player = GetComponent<RectTransform>();
+        player.SetParent(FindObjectOfType<PhotonManager>().bg);
+        player.anchoredPosition = Vector2.zero;
+    }
+    void Update()
+    {
+        if (!view.IsMine) return;
+        if (Input.GetKey(KeyCode.W))
+        {
+            player.anchoredPosition = new Vector2(player.anchoredPosition.x, player.anchoredPosition.y + 1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            player.anchoredPosition = new Vector2(player.anchoredPosition.x - 1, player.anchoredPosition.y);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            player.anchoredPosition = new Vector2(player.anchoredPosition.x, player.anchoredPosition.y - 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            player.anchoredPosition = new Vector2(player.anchoredPosition.x + 1, player.anchoredPosition.y);
+        }
+    }
+}
