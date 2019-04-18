a685a15420aed496e1ecf0493b634df86cdd1d96
NSWell <paris3@163.com>
2019-04-18 21:44:50 +0800

    update project

--- RunTogether/Assets/Scripts/UIScripts/LoginPage/WelcomePage.cs
+++ RunTogether/Assets/Scripts/UIScripts/LoginPage/WelcomePage.cs
@@ -13,64 +13,66 @@ namespace UIScripts.LoginPage
     {
         public override Widget build(BuildContext context)
         {
-            return new Stack(
-                fit: StackFit.expand,
-                children: new List<Widget>
-                {
-                    new Container(
-                        child: Image.asset("splashscreen", fit: BoxFit.cover)
+            return new StoreConnector<AppState, object>(
+                converter: (state) => null,
+                builder: (rootContext, rootModel, rootDispatcher) =>
+                    new Stack(
+                        children: new List<Widget>
+                        {
+                            Image.asset("local", fit: BoxFit.cover),
+                            new Container(
+                                margin: EdgeInsets.only(bottom: 35),
+                                child: new Row(
+                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                    crossAxisAlignment: CrossAxisAlignment.end,
+                                    children: new List<Widget>
+                                    {
+                                        new StoreConnector<AppState, object>(
+                                            converter: state => null,
+                                            builder: ((ctx, _, dispatcher) =>
+                                                new SizedBox(width: MediaQuery.of(context).size.width * .4f,
+                                                    child: new FlatButton(color: Colors.white,
+                                                        child: new Text("登录",
+                                                            style: CustomTheme.CustomTheme.DefaultTextThemen.display3),
+                                                        onPressed: () =>
+                                                        {
+                                                            dispatcher.dispatch(new LoginAction()
+                                                            {
+                                                                UserOpCode = UserOpCodeEnum.GoToLoginPage,
+                                                                Context = context
+                                                            });
+                                                        }
+                                                    )
+                                                )
+                                            ),
+                                            pure: true
+                                        ),
+                                        new StoreConnector<AppState, AppState>(
+                                            converter: state => state,
+                                            builder: ((ctx, model, dispatcher) =>
+                                                new SizedBox(width: MediaQuery.of(context).size.width * .4f,
+                                                    child: new FlatButton(color: Colors.green, child: new Text("注册",
+                                                            style: CustomTheme.CustomTheme.DefaultTextThemen.display2),
+                                                        onPressed: () =>
+                                                        {
+                                                            dispatcher.dispatch(new RegisterAction()
+                                                            {
+                                                                UserOpCode = UserOpCodeEnum.GoToRegisterPage,
+                                                                RequestOpCode = model.RequestOpCode,
+                                                                Context = context
+                                                            });
+                                                        }
+                                                    )
+                                                )
+                                            ),
+                                            pure: true
+                                        ),
+                                    }
+                                )
+                            ),
+                        }
                     ),
-                    new Container(
-                        margin: EdgeInsets.only(bottom: 35),
-                        child: new Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceAround,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: new List<Widget>
-                            {
-                                new StoreConnector<AppState, object>(
-                                    converter: state => null,
-                                    builder: ((ctx, _, dispatcher) =>
-                                        new SizedBox(width: MediaQuery.of(context).size.width * .4f,
-                                            child: new FlatButton(color: Colors.white,
-                                                child: new Text("登录",
-                                                    style: CustomTheme.CustomTheme.DefaultTextThemen.display3),
-                                                onPressed: () =>
-                                                {
-                                                    dispatcher.dispatch(new LoginAction()
-                                                    {
-                                                        UserOpCode = UserOpCodeEnum.GoToLoginPage,
-                                                        Context = context
-                                                    });
-                                                }
-                                            )
-                                        )
-                                    ),
-                                    pure: true
-                                ),
-                                new StoreConnector<AppState, AppState>(
-                                    converter: state => state,
-                                    builder: ((ctx, model, dispatcher) =>
-                                        new SizedBox(width: MediaQuery.of(context).size.width * .4f,
-                                            child: new FlatButton(color: Colors.green, child: new Text("注册",
-                                                    style: CustomTheme.CustomTheme.DefaultTextThemen.display2),
-                                                onPressed: () =>
-                                                {
-                                                    dispatcher.dispatch(new RegisterAction()
-                                                    {
-                                                        UserOpCode = UserOpCodeEnum.GoToRegisterPage,
-                                                        RequestOpCode = model.RequestOpCode,
-                                                        Context = context
-                                                    });
-                                                }
-                                            )
-                                        )
-                                    ),
-                                    pure: true
-                                ),
-                            }
-                        )
-                    ),
-                }
+                pure: true
             );
         }
     }
